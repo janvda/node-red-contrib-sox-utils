d4a88c270c7f16b16aa4d2522b5e3ceb508ec373
janvda <jan@Jans-MBP.lan>
2020-12-08 10:52:39 +0100

    corrected : node.info => node.log

--- sox_record.js
+++ sox_record.js
@@ -142,10 +142,10 @@ module.exports = function(RED) {
                     let options = msg.options.trim().split(" ");
                     let newArgArr = node.argArr.concat(options);
                     delete msg.options;
-                    node.info("spawn('sox',[" + newArgArr.toString() + "])");
+                    node.log("spawn('sox',[" + newArgArr.toString() + "])");
                     node.soxRecord = spawn("sox",newArgArr);
                 } else {
-                    node.info("spawn('sox',[" + node.argArr.toString() + "])");
+                    node.log("spawn('sox',[" + node.argArr.toString() + "])");
                     node.soxRecord = spawn("sox",node.argArr);
                 }
                 
